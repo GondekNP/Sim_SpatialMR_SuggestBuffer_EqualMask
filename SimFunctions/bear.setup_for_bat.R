e431d11c594fe885510cf5e2d4f9d05340459dd0
Nick Gondek <gonde012@umn.edu>
2017-10-21 18:51:24 -0500

    Simulations added.

--- SimFunctions/bear.setup_for_bat.R
+++ SimFunctions/bear.setup_for_bat.R
@@ -1,4 +1,4 @@
-source('D:/Sim_SpatialMR_1.1/SimFunctions/Simulation.R')
+source('D:/Sim_SpatialMR_SuggestBuffer_EqualMask/SimFunctions/Simulation.R')
 #source('C:/Users/Gonde/OneDrive/Documents/R Projects/Sim_SpatialMR_1.1/SimFunctions/Simulation.R')
 bear.setup() #This is called by the bat script
 
